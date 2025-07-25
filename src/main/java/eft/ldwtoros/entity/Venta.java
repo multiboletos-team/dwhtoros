eff2663fbb575f9c5a4be49e1ad9311053764570
user <mescalera@enerfueltech.com>
2025-07-25 12:51:08 -0700

    Otro intento

--- src/main/java/eft/ldwtoros/entity/Venta.java
+++ src/main/java/eft/ldwtoros/entity/Venta.java
@@ -12,6 +12,7 @@ public class Venta {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	
     private int orderId;
     private int eventId;
     private String eventDescription;
